35e3ef247c5a8aa97b44a1590cee40c7cd1567b0
seongeun <jdc05124@pusan.ac.kr>
2023-11-03 17:47:01 +0900

    form

---
+++ practice_articles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+# Create your models here.
---
+++ practice_articles/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,include
+from . import views
+
+
+
+app_name = 'articles'
+
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('create/',views.create,name='create'),
+    path('<int:id>/delete/',views.delete,name='delete'),
+    path('<int:id>/update/',views.update,name='update'),
+
+]
+
